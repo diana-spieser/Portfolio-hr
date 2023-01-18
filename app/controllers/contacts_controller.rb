a98427ad83d27f50ba2119ec26855eedc90424d9
diana spieser <dianaspieser@gmail.com>
2023-01-18 10:16:27 +0100

    ok contact form is working

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,5 @@
 class ContactsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :new, :sent]
   def new
     @contact = Contact.new
   end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :about, :services]
+  skip_before_action :authenticate_user!, only: [ :home, :about]
 
   def home
-
+    @contact = Contact.new
     @evgenia = Evgenium.all
     # The `geocoded` scope filters only flats with coordinates
     @markers = @evgenia.geocoded.map do |evgenium|
