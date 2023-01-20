9312d2ae5523a32060f79011255c65c1b73a7c0a
diana spieser <dianaspieser@gmail.com>
2023-01-20 09:58:40 +0100

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,16 @@ class PagesController < ApplicationController
 
     # The `geocoded` scope filters only flats with coordinates
   end
+  def create
+    @contact = Contact.new(params[:contact])
+      @contact.request = request
+    if @contact.deliver
+      redirect_to action: :sent
+    else
+      flash.now[:error] = 'Could not send message'
+          render :new, status: :unprocessable_entity
+    end
+  end
 
 
 end
